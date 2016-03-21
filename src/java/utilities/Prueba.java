30a2984d3f76d9e2141706699993f1555e357d96
Sebastian <Sebastian@192.168.1.12>
2016-03-21 14:08:24 -0500

    new java class cosa prueba

---
+++ src/java/utilities/Prueba.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package utilities;
+
+/**
+ *
+ * @author Sebastian
+ */
+public class Prueba {
+    
+}
